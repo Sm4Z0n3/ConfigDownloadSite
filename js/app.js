90c7c0cc20fc9beb8b502eae7631edb13656c3a6
SmaZone Ending <115725891+Sm4Z0n3@users.noreply.github.com>
2023-03-19 17:25:27 +0800

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,30 @@
+﻿function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
+   
+function dl_st(fname){
+  swal({
+    title: "确认",
+    text: "你确定要开始下载吗，这些文件可能含有外挂程序，所造成的全部后果需要您自己承担！",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  })
+  .then((willDelete) => {
+    if (willDelete) {
+      swal("好的，即将开始为您获取下载链接，请稍等片刻。" , {
+        icon: "success",
+        title:fname.name
+      });
+      window.location.assign("cfg/" + fname.name)
+    } else {
+      //取消下载
+    }
+  });
+}
---
+++ js/main_l.js
@@ -0,0 +1,46 @@
+﻿document.getElementById("usw").style = "background-color: white; opacity: 0.8; border: 0.5px solid white; border-radius: 20px; padding: 10px; margin-bottom: 10px;transition: background-color 0.2s ease-in-out;";
+document.getElementById("psw").style = "background-color: white; opacity: 0.8; border: 0.5px solid white; border-radius: 20px; padding: 10px; margin-bottom: 10px;transition: background-color 0.2s ease-in-out;";
+document.getElementById("mail").style = "background-color: white; opacity: 0.8; border: 0.5px solid white; border-radius: 20px; padding: 10px; margin-bottom: 10px;transition: background-color 0.2s ease-in-out;";
+document.getElementById("sub").style = "background-color: white; opacity: 1;border: 0.5px solid white; border-radius: 20px; width: 100px; height: 40px; margin-top: 10px; transition: background-color 0.2s ease-in-out;";
+document.getElementById("sub").onclick = "submita()";
+
+document.getElementById("type").style.display = "none";
+
+document.getElementById("submit").addEventListener("mouseout", function () {
+    this.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+});
+document.getElementById("submit").addEventListener("mouseover", function () {
+    this.style.backgroundColor = "rgba(179,179,164,1)";
+});
+
+document.getElementById("usw").addEventListener("mouseout", function () {
+    this.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+});
+document.getElementById("usw").addEventListener("mouseover", function () {
+    this.style.backgroundColor = "rgba(179,179,164,1)";
+});
+
+document.getElementById("psw").addEventListener("mouseout", function () {
+    this.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+});
+document.getElementById("psw").addEventListener("mouseover", function () {
+    this.style.backgroundColor = "rgba(179,179,164,1)";
+});
+
+document.getElementById("mail").addEventListener("mouseout", function () {
+    this.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+});
+document.getElementById("mail").addEventListener("mouseover", function () {
+    this.style.backgroundColor = "rgba(179,179,164,1)";
+});
+
+/*
+function submita() {
+    var usw = document.getElementById("usw").value;
+    var psw = document.getElementById("psw").value;
+    var url = "http://" + window.location.hostname + ":20775";
+    document.getElementById("form").action = url;
+    document.getElementById("form").method = "post";
+    console.log(url);
+}
+*/
